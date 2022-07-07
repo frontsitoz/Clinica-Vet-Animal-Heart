975f53a72c72c994e3f411b2bcd26bd1ccf92a01
Alvaro Prado <alvarojpt04.ws@gmail.com>
2022-07-07 18:53:26 -0500

    Cambios efectuados , poniendole un poco de js al index

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-const openModal = document.querySelector(".hero__cta");
-const modal = document.querySelector(".modal")
-const closeModal = document.querySelector(".modal__close")
+const openModal = document.querySelector(".hero__cta, .hero__cta2");
+const modal = document.querySelector(".modal, .modal2")
+const closeModal = document.querySelector(".modal__close, .modal__close2")
 
 openModal.addEventListener("click", (e)=>{
+    
 e.preventDefault();
     modal.classList.add("modal--show");
 });
